28004d502efe2055044d5b3641ce7337cbd5a8c1
Alexis-Perez <alexisperez00.cappi@gmail.com>
2021-07-05 15:14:50 -0500

    api-call exitoso, solo falta organizarlo

---
+++ src/components/weather-info.js
@@ -0,0 +1,9 @@
+// import Info from "./location-info"
+
+// const useLocation = async() => {
+//     let url = `http://api.weatherapi.com/v1/current.json?key=8a88406f21734422b8504112210507&q=${location.coordinates.lat},${location.coordinates.lng}&aqi=no`
+//     const response = await fetch(url).then(data => data.json())
+//     console.log(response)
+// }
+
+// export default useLocation
